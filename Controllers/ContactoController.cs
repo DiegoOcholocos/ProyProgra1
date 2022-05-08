29d4348303467e5162dfca18e57fca285a60183f
diegocho <diegoalonzo2001@hotmail.com>
2022-05-08 12:00:06 -0500

    conectarcontacto

--- Controllers/ContactoController.cs
+++ Controllers/ContactoController.cs
@@ -2,28 +2,38 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using appproy.Models;
 using Microsoft.AspNetCore.Mvc;
-
-namespace appproy.Controllers;
-
-        public class ContactoController: Controller
+using appproy.Models;
+using appproy.Data;
+namespace appcomics.Controllers
+{
+    public class ContactoController: Controller
     {
-            private readonly ILogger<ContactoController> _logger;
+        private readonly ILogger<ContactoController> _logger;
+        private readonly ApplicationDbContext _context;
 
-            public ContactoController(ILogger<ContactoController> logger)
-            {
-                _logger = logger;
-            }
-
+        public ContactoController(ApplicationDbContext context,
+            ILogger<ContactoController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
 
         public IActionResult Index()
         {
+            
             return View();
         }
-        public IActionResult Create()
-    {
-        return View("index");
-    }
-    }
 
+        [HttpPost]
+        public IActionResult Create(Contacto objContacto)
+        {
+            _context.Add(objContacto);
+            _context.SaveChanges();
+            ViewData["Message"] = "Se registro el contacto";
+            return View("Index");
+        }
+
+
+    }
+}
