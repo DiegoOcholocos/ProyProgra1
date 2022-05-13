d0312e43ca4724bd8d1015fb4181bf743cf6c316
diegocho <diegoalonzo2001@hotmail.com>
2022-05-13 14:07:56 -0500

    tabladata

---
+++ Models/DataUsers.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace appproy.Models
+{
+    [Table("t_DataUsers")]
+    public class DataUsers
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+
+        public int Id { get; set; }
+        [Column("Correo")]
+        public string? Correo { get; set; }
+        [Column("Name")]
+        public string? Name { get; set; }
+        [Column("LastName")]
+        public string? LastName { get; set; }
+        [Column("Phone")]
+        public string? Phone { get; set; }
+        [Column("Direction")]
+        public string? Direction { get; set; }
+    }
+}
