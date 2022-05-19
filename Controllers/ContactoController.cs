2d3e58a579acd2091b99ec72f24f3881e31603a0
diegocho <diegoalonzo2001@hotmail.com>
2022-05-19 10:42:58 -0500

    Contacto admin

--- Controllers/ContactoController.cs
+++ Controllers/ContactoController.cs
@@ -5,18 +5,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using appproy.Models;
 using appproy.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using appproy.Util;
+using Microsoft.AspNetCore.Identity;
+using System.Dynamic;
 namespace appproy.Controllers
 {
     public class ContactoController: Controller
     {
         private readonly ILogger<ContactoController> _logger;
         private readonly ApplicationDbContext _context;
-
+        private readonly UserManager<IdentityUser> _userManager; 
         public ContactoController(ApplicationDbContext context,
-            ILogger<ContactoController> logger)
+            ILogger<ContactoController> logger,
+            UserManager<IdentityUser> userManager)
         {
             _context = context;
             _logger = logger;
+            _userManager = userManager;
+
         }
 
         public IActionResult Index()
@@ -32,6 +40,19 @@ namespace appproy.Controllers
             _context.SaveChanges();
             ViewData["Message"] = "Se registro el contacto";
             return View("Index");
+        }
+
+
+
+        public async Task<IActionResult> Indexadmin(){
+        var userID = _userManager.GetUserName(User);
+        var items = from o in _context.DataContactos select o;
+        var datos = await items.ToListAsync();
+
+        dynamic model = new ExpandoObject();
+        model.elementosDatos = datos;
+
+        return View(model);
         }
 
 
