7eed9505065bddeac4ba933d664a75f2ca0bb2d1
diegocho <diegoalonzo2001@hotmail.com>
2022-06-08 00:55:44 -0500

    impresion final

--- Controllers/ImpresionController.cs
+++ Controllers/ImpresionController.cs
@@ -30,14 +30,46 @@ namespace appproy.Controllers
 
         }
 
-        public async Task<IActionResult> Index(){
+        public async Task<IActionResult> Index(string? searchString,string? searchString2){
             var items = from o in _context.DataPagos select o;
             items = items.Where(w => w.Status.Equals("PAGADO"));
-            items = items.Where(s => s.Curso.Contains("Confeccion Textil"));
+            items = items.Where(s => s.Curso.Contains(searchString));
+            items = items.Where(s => s.Mes_Matricula.Contains(searchString2));
             var datos1 = await items.OrderByDescending(w => w.Id).ToListAsync();
-
+    
+            String seleccurso = searchString;
+            var cursos1 = 0;
+            if (seleccurso == "Confeccion Textil")
+            {
+                cursos1 = 1;
+            }
+            else if (seleccurso == "Manejo de Maquinaria Industrial")
+            {
+                cursos1 = 2;
+            }
+            else if (seleccurso == "Diseño Grafico")
+            {
+                cursos1 = 3;
+            }
+            else if (seleccurso == "Mantenimiento de Equipos")
+            {
+                cursos1 = 4;
+            }
+            else if (seleccurso == "Curso de Excel")
+            {
+                cursos1 = 5;
+            }
+            else if (seleccurso == "Digitalizacion y Ofimatica")
+            {
+                cursos1 = 6;
+            }
+            else if (seleccurso == "Corte de Cabello")
+            {
+                cursos1 = 7;
+            }
             dynamic model = new ExpandoObject();
             model.elementosConfeccion = datos1;
+            model.elementocurso = cursos1;
 
             return View(model);
         }
